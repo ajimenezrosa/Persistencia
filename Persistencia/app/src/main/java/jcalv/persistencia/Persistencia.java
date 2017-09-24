826fdf8ff41ed0bef5e4a3ab76319885cd8c5bd3
Julio <jcalvarezg92@gmail.com>
2017-09-23 18:10:56 -0600

    fagmento login

---
+++ Persistencia/app/src/main/java/jcalv/persistencia/Persistencia.java
@@ -0,0 +1,15 @@
+package jcalv.persistencia;
+
+/**
+ * Created by jcalv on 23/09/2017.
+ */
+
+public class Persistencia  {
+
+    private Persistencia (){}
+
+    public static final String PREFERENCIA_NAME="nombre";
+
+    public static final String KEY_USERNAME="usuario";
+
+}
---
+++ Persistencia/app/src/main/java/jcalv/persistencia/SesionIniciada.java
@@ -0,0 +1,13 @@
+package jcalv.persistencia;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by jcalv on 23/09/2017.
+ */
+
+public class SesionIniciada extends Fragment {
+
+
+
+}
